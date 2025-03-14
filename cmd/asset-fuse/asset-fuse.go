2b011a112ba06bea7c97aa056b8365a27b92dc64
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-03-14 14:34:57 +0100

    update top-level TODO

--- cmd/asset-fuse/asset-fuse.go
+++ cmd/asset-fuse/asset-fuse.go
@@ -8,16 +8,14 @@ import (
 )
 
 // TODO:
-// - add flag handling
-// - add logging
 // - add subcommands for different operations
 //   - `asset-fuse mount` to mount the filesystem
-//   - `asset-fuse serve` to start a (local) unix domain socket server
-//   - `asset-fuse add` to add a file to the manifest
-//   - `asset-fuse get` to fetch uri(s) (thereby populating the [remote and/or local] cache with the asset(s), or simply to check if the asset is available and get the digest)
-//   - `asset-fuse put` to push a file to the remote asset service
-//   - `asset-fuse prefetch` to prefetch all assets in the manifest
-//   - `asset-fuse vendor` to copy all assets in the manifest to a directory or archive (for offline use without the asset-fuse filesystem)
+//   - `asset-fuse serve` to start a (local) unix domain socket server (what does this really expose? gRPC services? REST API? NFS?)
+//   - `asset-fuse push` to push a file (or directory) to the remote asset service
+//   - `asset-fuse download` to fetch uri(s) (thereby populating the [remote and/or local] cache with the asset(s)
+//   - `asset-fuse export` to copy all assets in the manifest to a directory or archive (for offline use without the asset-fuse filesystem)
+//   - `asset-fuse manifest add` to add a file to the manifest
+//   - `asset-fuse manifest update` to get the status of the asset-fuse service
 func main() {
 	root.Run(context.Background(), os.Args)
 }
